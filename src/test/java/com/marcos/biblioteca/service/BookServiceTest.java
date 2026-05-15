23ac6bdc21b09ca4771e056174c32d3ac2f3f9ec
Marcos <marcosv.java@gmail.com>
2026-05-15 14:46:53 -0400

    Added test about status in BookServiceTest

--- src/test/java/com/marcos/biblioteca/service/BookServiceTest.java
+++ src/test/java/com/marcos/biblioteca/service/BookServiceTest.java
@@ -1,4 +1,42 @@
 package com.marcos.biblioteca.service;
 
+import com.marcos.biblioteca.model.Book;
+import com.marcos.biblioteca.model.ReadingStatus;
+import com.marcos.biblioteca.repository.InMemoryBookRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class BookServiceTest {
+    BookService service;
+
+    @BeforeEach
+    void setUp() {
+         service = new BookService(new InMemoryBookRepository());
+    }
+
+    @Test
+    void cannotJumpFromWantingToRead_toRead() {
+        Book book = new Book("Title", "Author", "Category", ReadingStatus.I_WANT_TO_READ);
+        service.addBook(book);
+        Assertions.assertThrows(IllegalArgumentException.class, ()->
+                service.updateStatus(book, ReadingStatus.READ));
+
+    }
+    @Test
+    void cannotGoBackToWantingToRead_whenStatusIsRead() {
+        Book book = new Book("Title", "Author", "Category", ReadingStatus.READ);
+        service.addBook(book);
+        Assertions.assertThrows(IllegalArgumentException.class, ()->
+                service.updateStatus(book, ReadingStatus.I_WANT_TO_READ));
+    }
+    @Test
+    void cannotGoBackToReading_whenStatusIsRead() {
+        Book book = new Book("Title", "Author", "Category", ReadingStatus.READ);
+        service.addBook(book);
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                ()->
+                service.updateStatus(book, ReadingStatus.READING));
+    }
 }
